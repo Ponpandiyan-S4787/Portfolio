5212ed23139e4d9ebbafcc68e0a17423227547fe
suban shajahan <subanshajahan48@gmail.com>
2024-08-16 13:16:24 +0530

    create home ui

---
+++ projects/home-ui/src/lib/home-ui.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-home-ui',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      home-ui works!
+    </p>
+  `,
+  styles: ``
+})
+export class HomeUiComponent {
+
+}
---
+++ projects/home-ui/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of home-ui
+ */
+
+export * from './lib/home-ui.service';
+export * from './lib/home-ui.component';
