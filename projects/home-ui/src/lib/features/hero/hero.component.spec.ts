38dd968ec9a50d39a8126548b831fec5ca9deea1
suban shajahan <subanshajahan48@gmail.com>
2024-08-16 13:30:57 +0530

    create hero component

--- projects/home-ui/src/lib/features/hero/hero.component.spec.ts
+++ projects/home-ui/src/lib/features/hero/hero.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeUiComponent } from './home-ui.component';
+import { HeroComponent } from './hero.component';
 
-describe('HomeUiComponent', () => {
-  let component: HomeUiComponent;
-  let fixture: ComponentFixture<HomeUiComponent>;
+describe('HeroComponent', () => {
+  let component: HeroComponent;
+  let fixture: ComponentFixture<HeroComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeUiComponent]
+      imports: [HeroComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HomeUiComponent);
+    fixture = TestBed.createComponent(HeroComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
